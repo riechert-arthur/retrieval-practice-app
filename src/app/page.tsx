babcdfa955fd65c423e521119a7a13cd98b6bc85
Arthur Riechert <arthur@cybrco.ai>
2023-12-22 16:02:39 -0500

    Added timer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image'
+import Timer from '@/components/Timer'
 
 export default function Home() {
   return (
-    <main className='flex flex-col'>
+    <main className='flex flex-col justify-center items-center h-screen w-full'> 
+      <Timer />
     </main>
   )
 }
